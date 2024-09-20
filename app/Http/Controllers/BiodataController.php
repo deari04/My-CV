20898c36922c39f0d30c61d368772d740eec0406
Zura <dearisetiawan2004@gmail.com>
2024-09-20 10:27:39 +0800

    Initial commit after reinitialization

---
+++ app/Http/Controllers/BiodataController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\biodata;
+use Illuminate\Http\Request;
+
+class BiodataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('Biodata.create');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        // Validasi input
+        $validasi = $request->validate([
+            'nama' => 'required|string|max:255',
+            'keterangan' => 'required|string',
+            'umur' => 'required|string',
+            'alamat' => 'required|string',
+        ]);        
+
+        // Simpan data ke database
+        // Biodata::create([
+        //     'nama' => $request->input('nama'),
+        //     'keterangan' => $request->input('keterangan'),
+        //     'umur' => $request->input('umur'),
+        //     'alamat' => $request->input('alamat'),
+        // ]);
+        Biodata::create($validasi);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(biodata $biodata)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(biodata $biodata)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, biodata $biodata)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(biodata $biodata)
+    {
+        //
+    }
+}
---
+++ resources/views/Biodata/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">        
+        <h1>Form Biodata</h1>
+        <form method="POST" action="{{ route('biodata.store') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="nama" class="form-control" id="" aria-describedby="" placeholder="nama" name="nama" required>
+            </div>
+            <div class="mb-3">
+                <label for="keterangan" class="form-label">Keterangan</label>
+                <textarea type="keterangan" class="form-control" id="" aria-describedby="" placeholder="keterangan" name="keterangan" required></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="umur" class="form-label">Umur</label>
+                <input type="umur" class="form-control" id="" aria-describedby="" placeholder="umur" name="umur" required>
+            </div>
+            <div class="mb-3">
+                <label for="alamat" class="form-label">Alamat</label>
+                <input type="alamat" class="form-control" id="" aria-describedby="" placeholder="alamat" name="alamat" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
