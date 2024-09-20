b88c7fb98c1f976a69af4618c40d0be32026a776
Zura <dearisetiawan2004@gmail.com>
2024-09-20 13:43:56 +0800

    membuat migrasi

--- database/migrations/2024_09_20_052247_create_education_table.php
+++ database/migrations/2024_09_20_052247_create_education_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('education', function (Blueprint $table) {
             $table->id();
+            $table->year('tahun'); // Kolom untuk tahun
+            $table->string('sekolah'); // Kolom untuk nama sekolah
+            $table->string('jurusan'); // Kolom untuk jurusan
             $table->timestamps();
         });
     }
