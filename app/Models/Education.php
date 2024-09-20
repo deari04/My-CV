e3e380152a5df593b190b451bb8364ab92b35720
Zura <dearisetiawan2004@gmail.com>
2024-09-20 15:07:05 +0800

    membuat model education

--- app/Models/Education.php
+++ app/Models/Education.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Education extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'tahun',
+        'sekolah',
+        'jurusan',
+    ];
 }
