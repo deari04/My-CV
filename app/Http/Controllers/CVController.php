49872792deddf5ac5b70cc2327532f855d39a488
Zura <dearisetiawan2004@gmail.com>
2024-09-20 16:51:20 +0800

    menambahkan experience dan skills di CV Controller

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Biodata;
 use App\Models\Education;
+use App\Models\Experience;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class CVController extends Controller
@@ -11,6 +13,8 @@ class CVController extends Controller
     public function show() {
         $data = biodata::all();
         $education = Education::all();
-        return view('show', compact('data', 'education'));
+        $experience = Experience::all();
+        $skill = Skill::all();
+        return view('show', compact('data', 'education', 'experience', 'skill'));
     }
 }
